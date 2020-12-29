9746aa5310d62c6cb95b0a893e5df25632fa9d90
Severi Reivinen <severire>
2020-12-29 18:40:23 +0200

    Removed client email

--- public/mail.php
+++ public/mail.php
@@ -4,22 +4,22 @@ header("Content-Type: text/html; charset=UTF-8");
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
-$formcontent="Nimi: $name \nSähköpostiosoite: $email \nViesti: $message";
-$recipient = "karhunparrat@gmail.com";
-$subject = "Yhteydenottolomake";
-$mailheader = "From: dohzxqcn@karhunparrat.fi \r\n";
-if($name == ""){
-echo json_encode(['result' => false]);
+$formcontent="Nimi: $name \nSï¿½hkï¿½postiosoite: $email \nViesti: $message";   // E-mail content (name - email - message)
+$recipient = "";  // Your e-mail
+$subject = "Yhteydenottolomake";    // Mail subject
+$mailheader = "From: domain@domain \r\n";    // Mail header (From who)
+if($name == "") {
+    echo json_encode(['result' => false]);
 }
-else if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-echo json_encode(['result' => false]);
+else if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo json_encode(['result' => false]);
 }
-else if($message == ""){
-echo json_encode(['result' => false]);
+else if($message == "") {
+    echo json_encode(['result' => false]);
 }
 else if (mail($recipient, $subject, $formcontent, $mailheader)) {
-        echo json_encode(['result' => true]);
-    }else {
-        echo json_encode(['result' => false]);
- }
-?>
+    echo json_encode(['result' => true]);
+}
+else {
+    echo json_encode(['result' => false]);
+}
